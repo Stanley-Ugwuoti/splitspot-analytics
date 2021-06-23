d65244df695014181e62aa04f358a8c0b5524fcd
kamaldeen@splitspot.com <kamaldeen@splitspot.com>
2021-06-23 16:08:24 +0000

    - complete lead_profile table

--- models/marts/lead_profile.sql
+++ models/marts/lead_profile.sql
@@ -13,11 +13,34 @@ SELECT
 
   lead_timeline.duration_till_optimal_move_in,
   lead_timeline.age,
+
+  CASE WHEN age > 30 THEN 'Above 30'
+    WHEN age > 25 THEN '26-30'
+    WHEN age > 22 THEN '22-25'
+    WHEN age > 17 THEN '18-22'
+ END
+  AS age_category,
+
   lead_timeline.occupation,
   lead_timeline.apartment_of_interest,
   lead_timeline.applications_date,
   lead_timeline.application_source,
   lead_timeline.application_length_of_stay as applications_intended_length_of_stay,
+  
+  CASE WHEN lead_timeline.application_length_of_stay = '3 Months (summer only)'  THEN '3-5 months'
+    WHEN lead_timeline.application_length_of_stay = '4 Months'  THEN '3-5 months'
+    WHEN lead_timeline.application_length_of_stay = '5 Months'  THEN '3-5 months'
+    WHEN lead_timeline.application_length_of_stay = '6 Months'  THEN '6-9 months'
+    WHEN lead_timeline.application_length_of_stay = '7 Months'  THEN '6-9 months'
+    WHEN lead_timeline.application_length_of_stay = '8 Months'  THEN '6-9 months'
+    WHEN lead_timeline.application_length_of_stay = '9 Months'  THEN '6-9 months'
+    WHEN lead_timeline.application_length_of_stay = '10 Months'  THEN '10 months and above'
+    WHEN lead_timeline.application_length_of_stay = '11 Months'  THEN '10 months and above'
+    WHEN lead_timeline.application_length_of_stay = '12+ Months'  THEN '10 months and above'
+       
+    ELSE 'Not Specified' END
+  AS estimated_rent_period_category,
+
   lead_timeline.rsvps_date,
   lead_timeline.event_type,
   lead_timeline.roommate_introductions_date,
@@ -29,8 +52,27 @@ SELECT
   lead_timeline.lease_signed_address,
 
   lead_timeline.wait_period,
+
+  CASE WHEN wait_period > 90 THEN 'over 3 months'
+    WHEN wait_period > 60 THEN '2-3 months'
+    WHEN wait_period > 30 THEN '1-2 months'
+    WHEN wait_period > 10 THEN '11-29 days'
+    WHEN wait_period < 10 THEN '0-10'
+    ELSE 'Not Specified' END
+  AS wait_period_category,
+
   lead_timeline.lease_source,
   lead_timeline.conversion_duration,
+
+  CASE WHEN conversion_duration > 30 THEN '30+ days'
+    WHEN conversion_duration > 20 THEN '21-30'
+    WHEN conversion_duration > 15 THEN '16-20'
+    WHEN conversion_duration > 10 THEN '11-15'
+    WHEN conversion_duration > 5 THEN '5-10'
+    WHEN conversion_duration > 0 THEN '1-5'
+    ELSE 'Not Specified' END
+  AS conversion_duration_category,
+
   lead_timeline.lease_length_of_stay as lease_intended_rent_duration,
 
   buildium_data.leaseid,
