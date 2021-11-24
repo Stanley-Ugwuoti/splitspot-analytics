27caaf2213bd872b8a265efe2393a3150b464472
joe@splitspot.com <joe@splitspot.com>
2021-11-24 18:52:42 +0000

    Customer Acquisition Cost Table

---
+++ models/staging/financial_data/stg_customer_acquisition_cost.sql
@@ -0,0 +1,8 @@
+SELECT 
+DATE(date_) AS date,
+Category,
+type,
+Classification,
+item,
+Costs
+FROM `natural-rider-307113.Financials.customer_acquisition_cost`
---
+++ models/staging/financial_data/stg_ramp_up_details.sql
@@ -0,0 +1,10 @@
+-- Ramp up end dates by apartments
+
+SELECT 
+UPPER(term) = 'RS' AS Revenue_share,
+apartment_id,
+CASE 
+    WHEN ramp_up_end_date IN ('','N/A') THEN NULL 
+    ELSE CAST(CAST(ramp_up_end_date AS TIMESTAMP) AS DATE)
+END AS ramp_up_end_date,
+FROM `natural-rider-307113.Financials.ramp_up_details`
