1d03022263c5c43938acc036e285541f9774f236
Audrey <kemiaudrey@gmail.com>
2021-12-01 22:31:42 +0100

    removing unused file

--- models/marts/Financials/Customer_acquisition_cost.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-EXTRACT(MONTH FROM date) AS month,
-type,
-category,
-ROUND(SUM(Costs),2) AS total_cost
-FROM {{ ref ('stg_customer_acquisition_cost')}}
-GROUP BY month,type,category
-ORDER BY month,type
