c2f534817ed2f9c058206c843c808523763472c7
Audrey <kemiaudrey@gmail.com>
2021-12-01 22:15:17 +0100

    New property cac model

---
+++ models/marts/Financials/property_cac.sql
@@ -0,0 +1,31 @@
+-- Property Customer Acqusition cost aggregated by months
+
+WITH property_cost AS(
+    SELECT
+        DATE_TRUNC(date,MONTH) AS month_start,
+        ROUND(SUM(Costs),2) AS total_cost
+    FROM {{ ref ('stg_customer_acquisition_cost')}}
+    WHERE type = 'Property'
+    GROUP BY 1
+),
+apt_onboarded_grouped AS (
+   SELECT
+        DATE_TRUNC(date_onboarded,MONTH) AS start_month,
+        SUM(no_of_rooms) AS rooms_onboarded  
+   FROM {{ ref ('apartment_onboarded')}}
+   GROUP BY 1
+
+-- To group on-boarded apartments by month start for easy joining
+),
+property_cac AS (
+    SELECT 
+        * 
+    FROM property_cost
+    LEFT JOIN  apt_onboarded_grouped AS apartment_onboarded
+    ON property_cost.month_start = apartment_onboarded.start_month
+)
+SELECT 
+    month_start,
+    total_cost,
+    rooms_onboarded
+FROM property_cac
