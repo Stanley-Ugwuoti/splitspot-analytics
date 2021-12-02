d41412daa7e8113abeb60126cefeae2b5777ecf3
Audrey <kemiaudrey@gmail.com>
2021-12-02 21:13:00 +0100

    updated styling changes on tenant cac model

--- models/marts/Financials/tenant_cac.sql
+++ models/marts/Financials/tenant_cac.sql
@@ -2,11 +2,11 @@
 
 WITH tenant_cost AS (
     SELECT
-        DATE_TRUNC(date,MONTH) AS month_start,
-        ROUND(SUM(Costs),2) AS total_cost
+        DATE_TRUNC(date, MONTH) AS month_start,
+        ROUND(SUM(cost), 2) AS total_cost
     FROM {{ ref ('stg_customer_acquisition_cost')}}
-WHERE type = 'Tenant'
-GROUP BY 1
+    WHERE type = 'Tenant'
+    GROUP BY 1
 ),
 leases AS(
     SELECT
@@ -14,16 +14,11 @@ leases AS(
         SUM(no_of_leases) AS lease_count,
     FROM {{ ref ('leases_signed_source')}}
     GROUP BY 1
-),
-tenant_cac AS(   
-    SELECT 
-        * 
-    FROM leases 
-    LEFT JOIN tenant_cost AS tenant_cac
-    ON leases.start_month = tenant_cac.month_start
 )
     SELECT 
         start_month,
         total_cost,
         lease_count
-    FROM tenant_cac
+    FROM leases 
+    LEFT JOIN tenant_cost AS tenant_cac ON leases.start_month = tenant_cac.month_start
+
