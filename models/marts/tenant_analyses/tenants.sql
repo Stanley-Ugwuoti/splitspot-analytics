7beb995dc72c37885071fd27845a3305d8645aa0
Audrey <kemiaudrey@gmail.com>
2021-12-09 21:57:54 +0100

    Updates to tenants mart table and buildium tenancy table

--- models/marts/tenant_analyses/tenants.sql
+++ models/marts/tenant_analyses/tenants.sql
@@ -4,29 +4,26 @@ WITH
     buildium_tenant_id,
     MIN(start_date) AS start_date,
     MAX(end_date) AS estimated_end_date,
-    MIN(DATE_TRUNC(start_date, MONTH)) AS cohort_month
+    MIN(DATE_TRUNC(start_date, MONTH)) AS start_month
   FROM
     {{ ref('stg_tenant_rent_charge_by_month') }}
   GROUP BY 1
-  ORDER BY
-    cohort_month 
 ),
   buildium_data AS (
   SELECT
     *
   FROM
-    {{ ref('stg_buildium_tenancy') }}
+    {{ ref('buildium_tenancy') }}
 )
 SELECT
   cohort.buildium_tenant_id,
-  cohort_month,
+  start_month,
   full_name,
-  customer_id AS customer_email,
+  customer_id AS email,
   address_id,
   buildium_data.start_date,
   end_date,
-  estimated_end_date,
-  date_today
+  estimated_end_date
 FROM
   cohort
 LEFT JOIN buildium_data ON cohort.buildium_tenant_id = buildium_data.buildium_tenant_id
