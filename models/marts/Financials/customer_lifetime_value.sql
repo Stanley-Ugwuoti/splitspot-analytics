5c1eb29d6cc87c9195177c48b3eee13761fa13c7
Audrey <kemiaudrey@gmail.com>
2021-12-13 20:31:18 +0100

    Ltv formatting changes and stg table change

--- models/marts/Financials/customer_lifetime_value.sql
+++ models/marts/Financials/customer_lifetime_value.sql
@@ -1,14 +1,18 @@
- SELECT
-    tenant_charges.buildium_tenant_id,
-    earliest_month_start,
-    ROUND(SUM(rent_charge),2) AS rent_charge,
-    ROUND(SUM(estimated_revenue),2) AS estimated_revenue,
-    ROUND(SUM(DATE_DIFF(tenant_charges.end_date,start_date,DAY)/30),2) AS length_of_stay,
-    MAX(tenant_charges.end_date) AS estimated_end_date
-  FROM
-    {{ ref ('stg_tenant_rent_charge_by_month') }} AS tenant_charges
-  LEFT JOIN
-    {{ ref ('tenants') }} AS tenants
-  ON tenant_charges.buildium_tenant_id = tenants.buildium_tenant_id
-    GROUP BY 1,2
-    ORDER BY length_of_stay DESC
+WITH 
+  tenant_charges AS (
+    SELECT * FROM {{ ref ('stg_tenant_rent_charge_by_month') }} 
+  ),
+  tenants AS (
+    SELECT * FROM {{ ref ('tenants') }}
+  )
+SELECT
+  tenant_charges.buildium_tenant_id,
+  earliest_month_start,
+  ROUND(SUM(rent_charge),2) AS total_rent_charge,
+  ROUND(SUM(estimated_revenue),2) AS total_estimated_revenue,
+  ROUND(SUM(DATE_DIFF(tenant_charges.end_date,start_date,DAY)/30),2) AS length_of_stay,
+  MAX(tenant_charges.end_date) AS estimated_end_date
+FROM tenant_charges
+LEFT JOIN tenants ON tenant_charges.buildium_tenant_id = tenants.buildium_tenant_id
+GROUP BY 1, 2
+ORDER BY earliest_month_start, length_of_stay DESC
