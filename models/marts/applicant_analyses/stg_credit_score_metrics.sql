30c5fc5344e0af6a61eacc8c253ed8c8275920e5
Stanley <aristotlebance@gmail.com>
2021-12-17 15:16:04 +0100

    creating staging table for credit score metrics

---
+++ models/marts/applicant_analyses/stg_credit_score_metrics.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CAST(date_completed AS DATE) AS date_completed,
+    LOWER(email) AS email,
+    name AS applicant_name,
+    approval_mode, applicant_credit_score, applicant_no_of_missing_payment,
+    cosigner_credit_score, cosigner_no_of_missing_payment, lease_sent, lease_signed
+
+FROM {{source('customer', 'credit_score_metrics')}}
+
+WHERE date_completed IS NOT NULL AND lease_sent IS NOT NULL AND lease_signed IS NOT NULL
