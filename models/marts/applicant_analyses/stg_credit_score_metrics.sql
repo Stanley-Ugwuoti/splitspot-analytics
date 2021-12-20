a632636546260457d8a70c38534e6d789e283319
Stanley <aristotlebance@gmail.com>
2021-12-20 22:58:45 +0100

    Addedd CTE at the top of the query.

--- models/marts/applicant_analyses/stg_credit_score_metrics.sql
+++ models/marts/applicant_analyses/stg_credit_score_metrics.sql
@@ -1,8 +1,17 @@
+WITH 
+modified_column_names 
+AS (SELECT 
+    applicant_credit_score AS acs,
+    cosigner_credit_score AS ccs,
+    *
+FROM {{source('customer', 'credit_score_metrics')}})
+
+
 SELECT 
     CAST(date_completed AS DATE) AS date_completed,
     LOWER(email) AS email,
     name AS applicant_name,
-    # 'Approval mode' just splits whether the applicant either submitted documents or underwent a credit/background check.
+    # Approval mode just tells us whether the applicant either submitted documents or underwent a credit/background check.
     approval_mode,
 
     CAST(applicant_no_of_missing_payment AS INTEGER) AS applicant_no_of_missing_payments,
@@ -20,7 +29,6 @@ SELECT
         ELSE ccs
     END AS cosigner_credit_score,
 
-FROM (SELECT *, applicant_credit_score AS acs, cosigner_credit_score AS ccs
-      FROM {{source('customer', 'credit_score_metrics')}})
+FROM modified_column_names
 
 WHERE date_completed IS NOT NULL AND lease_sent IS NOT NULL AND lease_signed IS NOT NULL
