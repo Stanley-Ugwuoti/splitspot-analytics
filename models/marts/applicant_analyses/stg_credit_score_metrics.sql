17fed32907c50cde870f422ed8d6284a9a16f28b
Stanley <aristotlebance@gmail.com>
2021-12-20 16:06:25 +0100

    put all column names onto a new line.

--- models/marts/applicant_analyses/stg_credit_score_metrics.sql
+++ models/marts/applicant_analyses/stg_credit_score_metrics.sql
@@ -2,8 +2,12 @@ SELECT
     CAST(date_completed AS DATE) AS date_completed,
     LOWER(email) AS email,
     name AS applicant_name,
-    approval_mode, applicant_credit_score, applicant_no_of_missing_payment,
-    cosigner_credit_score, cosigner_no_of_missing_payment, lease_sent, lease_signed
+    approval_mode,
+    applicant_credit_score,
+    applicant_no_of_missing_payment,
+    cosigner_credit_score,
+    cosigner_no_of_missing_payment,
+    lease_sent, lease_signed
 
 FROM {{source('customer', 'credit_score_metrics')}}
 
